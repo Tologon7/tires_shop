b3d56f58fdc21bc630fe2bbad022aa99ee77551a
Artur <arturunkulov30@gmail.com>
2025-03-05 13:49:35 +0600

    swagger update code all sagger

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from drf_yasg import openapi
 from rest_framework import permissions
 from rest_framework.permissions import AllowAny
 from django.views.decorators.csrf import csrf_exempt
-
+from django.contrib.auth import views as auth_views
 # Создаем объект schema_view для Swagger
 schema_view = get_schema_view(
     openapi.Info(
@@ -21,15 +21,18 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=(AllowAny,),  # Разрешаем доступ всем
 )
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/', include("product.urls")),
-    path('swagger/', csrf_exempt(schema_view.with_ui('swagger', cache_timeout=0)), name='swagger-docs'),
-    path('swagger.json/', csrf_exempt(schema_view.without_ui(cache_timeout=0)), name='schema-json'),
-    path('redoc/', csrf_exempt(schema_view.with_ui('redoc', cache_timeout=0)), name='schema-redoc'),
-]
 
+    # Swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-docs'),
+    path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # Аутентификация
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
 # Статические файлы
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
